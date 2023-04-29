d2615edf7d168364b1af0cc68d215dc560334a3e
Diana <dianajohnson13@gmail.com>
2023-04-29 13:03:13 -0700

    Add API call to Pa11y

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pa11y = require("pa11y");
 
 const PORT = process.env.PORT || 3001;
 
@@ -6,4 +7,13 @@ const app = express();
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+app.get('/test', async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: 'Missing URL' })
+  } else {
+    const results = await pa11y(req.query.url)
+    res.status(200).json(results)
+  }
 });
