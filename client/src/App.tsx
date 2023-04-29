d83639efae200bc66147041ac01cf6233db969bf
Diana <dianajohnson13@gmail.com>
2023-04-29 12:50:06 -0700

    Initialize client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        Hello world
+      </main>
+    </div>
+  );
+}
+
+export default App;
