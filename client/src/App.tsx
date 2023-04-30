cf27f3de8a619f735b037e1e5c95557c692bd606
Diana <dianajohnson13@gmail.com>
2023-04-30 09:20:04 -0700

    Get basic call to server and pa11y working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,42 @@
 import './App.css';
+import { useState, FormEvent } from 'react';
+
+const fetchUrl = async (newURL: string) => {
+  const resp = await fetch(`/api/test?url=${newURL}`);
+  return resp.json();
+}
 
 function App() {
+let [ data, setData ] = useState<any>();
+let [ loading, setLoading ] = useState<boolean>(false);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    let newURL = (document.getElementById('urlInput') as HTMLInputElement).value;
+    if (newURL === "") { // handle invalid URL too
+      // show error message under input
+    } else {
+      fetchUrl(newURL)
+        .then(data => {
+          console.log(data)
+          setData(data);
+        });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <main>
-        Hello world
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="urlInput">
+            {"Enter a URL: "}
+            <input id="urlInput" />
+          </label>
+          <button type="submit">Run Test</button>
+        </form>
+
       </main>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const pa11y = require("pa11y");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
-app.get('/test', async (req, res) => {
+app.get('/api/test', async (req, res) => {
   if (!req.query.url) {
-    res.status(400).json({ error: 'Missing URL' })
+    res.status(400).json({ error: 'Missing URL' });
   } else {
-    const results = await pa11y(req.query.url)
-    res.status(200).json(results)
+    const results = await pa11y(req.query.url);
+    res.status(200).json({results});
   }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
