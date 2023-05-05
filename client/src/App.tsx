4110b61d389318b9a2aa5cfc1318565adb3ae62d
Diana <dianajohnson13@gmail.com>
2023-05-05 15:17:44 -0700

    Render results and Add loading state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,30 @@
 import './App.css';
 import { useState, FormEvent } from 'react';
 
+interface ResultIssue {
+  code: string;
+  context: string;
+  message: string;
+  selector: string;
+  type: string;
+  typeCode: number;
+};
+
+interface Result {
+  results: {
+    issues: ResultIssue[],
+    documentTitle: string,
+    pageUrl: string
+  }
+}
+
 const fetchUrl = async (newURL: string) => {
   const resp = await fetch(`/api/test?url=${newURL}`);
   return resp.json();
 }
 
 function App() {
-let [ data, setData ] = useState<any>();
+let [ data, setData ] = useState<Result>();
 let [ loading, setLoading ] = useState<boolean>(false);
 
   const handleSubmit = (event: FormEvent) => {
@@ -16,10 +33,16 @@ let [ loading, setLoading ] = useState<boolean>(false);
     if (newURL === "") { // handle invalid URL too
       // show error message under input
     } else {
+      setLoading(true);
       fetchUrl(newURL)
         .then(data => {
           console.log(data)
           setData(data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(error.message);
+          setLoading(false);
         });
     }
   }
@@ -36,7 +59,29 @@ let [ loading, setLoading ] = useState<boolean>(false);
           </label>
           <button type="submit">Run Test</button>
         </form>
+        {loading && <div>Loading...</div>}
 
+
+        {data && (
+          <div>
+            <table>
+              <tr>
+                <th>Code</th>
+                <th>Context</th>
+                <th>Message</th>
+              </tr>
+              {data.results.issues.map((issue: ResultIssue, idx: number) => {
+                return (
+                  <tr key={idx}>
+                    <td>{issue.code}</td>
+                    <td>{issue.context}</td>
+                    <td>{issue.message}</td>
+                  </tr>
+                )
+              })}
+            </table>
+          </div>
+        )}
       </main>
     </div>
   );
