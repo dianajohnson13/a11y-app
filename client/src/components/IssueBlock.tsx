74b094d2d6886d2086581fe332ac1142e796c293
Diana <dianajohnson13@gmail.com>
2023-05-12 10:48:06 -0700

    Add functional Show all/Show less buttons

--- client/src/components/IssueBlock.tsx
+++ client/src/components/IssueBlock.tsx
@@ -1,9 +1,18 @@
 import '../styles/IssueBlock.css';
 
 import { IssueGroup } from '../App';
+import { useState } from 'react';
 
 interface Props {
     issue: IssueGroup
+}
+
+function Instance({instance}: {instance: string}) {
+    return (
+        <p className='issue-instance'>
+            <code>{instance}</code>
+        </p>
+    );
 }
 
 export default function IssueBlock({
@@ -13,19 +22,31 @@ export default function IssueBlock({
         instances
     }
 }: Props) {
+    const [showAll, setShowAll] = useState<boolean>(false)
 
     return (
         <div className='issue' key={code}>
             <p className='issue-message'>{message}</p>
-            <p className='issue-content'>
-            <code>{instances[0]}</code>
-            </p>
-            {instances.length > 1 ? (
-                <p key={code}>
-                    {`We found ${instances.length - 1} more instance${instances.length > 2 ? 's' : ''} of this  `}
-                    <a>See all</a>
-                    </p>
-            ) : null}
+            <div className='issue-instance-list'>
+                {showAll ? (
+                    <>
+                        {instances.map(instance => (
+                            <Instance instance={instance} key={instance} />
+                        ))}
+                        <button className='secondary-btn' onClick={() => setShowAll(false)}>Show less</button>
+                    </>
+                ) : (
+                    <>
+                        <Instance instance={instances[0]} />
+                        {instances.length > 1 ? (
+                            <p>
+                                {`We found ${instances.length - 1} more instance${instances.length > 2 ? 's' : ''} of this issue. `}
+                                <button className='secondary-btn' onClick={() => setShowAll(true)}>Show all</button>
+                            </p>
+                        ) : null}
+                    </>
+                )}
+            </div>
             <p className='issue-code'>{code}</p>
         </div>
     );
