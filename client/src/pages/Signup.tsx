ecd682f03c9cae1c9dadc500750798efb10d29ce
Diana <dianajohnson13@gmail.com>
2023-06-13 10:18:13 -0700

    Minimal form styling

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -2,26 +2,31 @@ import '../styles/Auth.css';
 
 export default function Signup() {
   return (
-    <form>
+    <form className='auth-form'>
         <div>
             <h1>Sign Up</h1>
             <p>Please complete this form to create an account.</p>
 
-            <label htmlFor="email"><b>Name</b></label>
-            <input type="text" placeholder="Name" name="email" required />
+            <div className="auth-field required">
+              <label htmlFor="email"><b>Name</b></label>
+              <br/>
+              <input type="text" name="email" required />
+            </div>
 
-            <label htmlFor="email"><b>Email</b></label>
-            <input type="text" placeholder="Email" name="email" required />
+            <div className="auth-field required">
+              <label htmlFor="email"><b>Email</b></label>
+              <br/>
+              <input type="text" name="email" required />
+            </div>
 
-            <label htmlFor="psw"><b>Password</b></label>
-            <input type="password" placeholder="Password" name="password" required />
+            <div className="auth-field required">
+              <label htmlFor="password"><b>Password</b></label>
+              <br/>
+              <input type="password" name="password" required />
+            </div>
 
-            <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Password" name="password-repeat" required/>
-
-            <div>
-            <button type="button">Cancel</button>
-            <button type="submit">Sign Up</button>
+            <div className='footer'>
+              <button className="primary-btn" type="submit">Sign Up</button>
             </div>
         </div>
     </form>
